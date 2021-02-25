8161b5c28d0564e4c2a21bfdf49ab4e86e5357c9
NuryanaNn <nuryan504@gmail.comescgit clear>
2021-02-25 15:11:55 +0700

    Initial Commit

---
+++ application/controllers/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+class Auth extends CI_Controller{
+    public function index()
+    {
+        $this->load->view('admionistrator/login');
+        $this->load->view('admionistrator/login');
+    }
+
+}
+
+
+
+?>
